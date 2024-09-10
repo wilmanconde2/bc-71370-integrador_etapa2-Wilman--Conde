268054e50d57ad07d9a31f982956beb52d7a19a7
Wilman Conde <willconde2@hotmail.com>
2024-09-09 20:35:48 -0500

    full

--- main.js
+++ main.js
@@ -9,10 +9,7 @@ window.addEventListener('DOMContentLoaded', async function () {
       throw new Error('No se pudo obtener la plantilla');
     }
     const plantilla = await respuesta.text();
-    // console.log(plantilla);
-
     const template = Handlebars.compile(plantilla);
-
     template({ plantilla: plantilla });
 
     // ! local host db.json
@@ -23,11 +20,8 @@ window.addEventListener('DOMContentLoaded', async function () {
       throw new Error('Algo paso con los productos', respuestaBackend.status);
     }
     const dataProductos = await respuestaBackend.json();
-    // console.log(dataProductos);
     const data = { productos: dataProductos };
     const html = template(data);
-
-    // console.log(html);
 
     const contenedorCards = document.querySelector('#contenedorCards');
     if (!contenedorCards) {
